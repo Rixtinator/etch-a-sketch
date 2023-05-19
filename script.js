2cbcc836c213f2b6eb99c64176d3477028631eba
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2023-05-19 09:13:41 +0200

    add event listener to draw blocks (divs) in container

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 const container = document.getElementById("container");
-
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (c = 0; c < rows * cols; c++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
+    }
+
+    let gridItems = document.getElementsByClassName("grid-item");
+    let trigger = false;
+
+    this.addEventListener('mousedown', function () {
+        trigger = true;
+    });
+
+    this.addEventListener('mouseup', function () {
+        trigger = false;
+    });
+
+    let coloring = function () {
+        if (trigger === true) {
+            this.style.backgroundColor = "black";
+        }
+    };
+
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].addEventListener("mousemove", coloring, false);
     };
 };
 
 makeRows(16, 16);
-
-let gridItems = document.getElementsByClassName("grid-item");
-
-let coloring = function () {
-    console.log(gridItems);
-};
-
-for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].addEventListener('mouseover', coloring, false);
-};
